b6987755e49f3d287d8537f946c0c73545592c62
chandraroy08 <68724499+chandraroy08@users.noreply.github.com>
2021-07-03 18:06:27 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+const Engine=Matter.Engine;
+const World=Matter.World;
+const Bodies=Matter.Bodies;
+
+var engine,world;
+var bgimg;
+var snow=[];
+var snow1img,snow2img;
+
+function preload(){
+  bgimg=loadImage("snow3.jpg");
+  snow1img=loadImage("snow4.webp");
+  snow2img=loadImage("snow5.webp");
+}
+function setup() {
+  
+      
+    }
+
+  
+ 
+
+
+
+function draw() {
+  
+  
+ 
+  
+}
+
